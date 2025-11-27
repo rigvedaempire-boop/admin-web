4a9bcaf3c411901f78bd9814c4c80fb4c5a6507d
Ritesh Pandey <riteshpandey@MacBookAir.lan>
2025-11-27 22:21:10 +0530

    server point change

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3025,
     proxy: {
       '/api': {
-        target: 'http://localhost:5010',
+        target: 'http://localhost:5025',
         changeOrigin: true
       }
     }
